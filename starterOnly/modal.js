88099406fc99d15b4d117ed433268301e669f3c8
Malek <absimalekmootez@gmail.com>
2023-03-25 22:06:40 +0100

    fermeture de modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,33 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
+
+const modalClose = document.getElementsByClassName('close');
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
+//close modal
+
+modalClose[0].addEventListener('click', closeModal);
 
 
+function closeModal() {
+  modalbg.style.display = 'none';
+  trigger.preventDefault();
+  
+}
