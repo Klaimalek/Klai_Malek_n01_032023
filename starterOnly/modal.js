81f36a97400911afa3a772b13c4294666ef21d7e
Malek <absimalekmootez@gmail.com>
2023-03-27 12:37:41 +0200

    modale de validation d'inscription

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,9 +10,12 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
+//récupérer les données de formulaire
 const formData = document.querySelectorAll('.formData');
-
+// récupérer le span close
 const modalClose = document.getElementsByClassName('close');
+//récupérer le button c'est partie
+const modalInscription = document.getElementsByClassName('button');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
@@ -31,3 +34,13 @@ function closeModal() {
   trigger.preventDefault();
   
 }
+// modale validation de l'inscription
+modalInscription.addEventListener('click',ValidationInscription);
+
+function ValidationInscription() {
+ if(formData.value() = ""){
+  e.preventDefault();
+  modalbg.style.display = 'block';
+ }
+}
+
